a7f6a44b2c7c10031b3edbfe072b7ee377cea720
Yasrul <yasrul@gmail.com>
2018-08-15 16:12:47 +0800

    20190815

---
+++ sites/default/files/php/twig/5b738da8a5055_menu-local-tasks.html.twi_qQnEsAEBbjdQavpueEg5si4yM/cI44np6Oh4qIMggb8ZytKC1XTLs2GSWqjnDOeH3GEEA.php
@@ -0,0 +1,95 @@
+<?php
+
+/* themes/cloud_zymphonies_theme/templates/navigation/menu-local-tasks.html.twig */
+class __TwigTemplate_549281da5413e198f7fb18858a16d2f0417160a54779065e65efd0bd102f9c69 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 16);
+        $filters = array("t" => 17);
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('if'),
+                array('t'),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 16
+        if (($context["primary"] ?? null)) {
+            // line 17
+            echo "  <h2 class=\"visually-hidden\">";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Primary tabs")));
+            echo "</h2>
+  <ul class=\"tabs primary\">";
+            // line 18
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["primary"] ?? null), "html", null, true));
+            echo "</ul>
+";
+        }
+        // line 20
+        if (($context["secondary"] ?? null)) {
+            // line 21
+            echo "  <h2 class=\"visually-hidden\">";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Secondary tabs")));
+            echo "</h2>
+  <ul class=\"tabs secondary\">";
+            // line 22
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["secondary"] ?? null), "html", null, true));
+            echo "</ul>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/cloud_zymphonies_theme/templates/navigation/menu-local-tasks.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  62 => 22,  57 => 21,  55 => 20,  50 => 18,  45 => 17,  43 => 16,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "themes/cloud_zymphonies_theme/templates/navigation/menu-local-tasks.html.twig", "/home/yasrul/projects/ikadintb/themes/cloud_zymphonies_theme/templates/navigation/menu-local-tasks.html.twig");
+    }
+}
